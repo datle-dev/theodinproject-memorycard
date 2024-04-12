70a8b38e8cfb44fdfd0bccf01159497264050514
datle-dev <dle.dev@outlook.com>
2024-04-12 10:06:48 -0400

    Add basic API fetch with useEffect and display response

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './styles/App.css';
+import ScoreBoard from './components/ScoreBoard';
 
 function App() {
-  return <h1>Hello world!</h1>;
+  const [pokemon, setPokemon] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [points, setPoints] = useState(0);
+  const [best, setBest] = useState(points);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      let response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+      let data = await response.json();
+      setPokemon(data);
+      setIsLoading(false);
+    }
+    fetchPokemon();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <p>Loading...</p>
+    );
+  } else {
+    return (
+      <>
+        <h1>Pokemon Memory Card</h1>
+        <p>Earn points by clicking on Pokemon, but don&apos;t click on any Pokemon more than once!</p>
+        <ScoreBoard points={points} best={best} />
+        <article>
+          <img src={pokemon.sprites.front_default} alt={pokemon.species.name} />
+          <h2>{pokemon.species.name}</h2>
+        </article>
+      </>
+    );
+  }
 }
 
 export default App;
