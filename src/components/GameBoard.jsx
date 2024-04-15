77cc9470a6a3bda3095a82173479a6783cb0304c
datle-dev <dle.dev@outlook.com>
2024-04-15 09:44:28 -0400

    Modify GameBoard with CSS classnames and utility
    Wrap returned JSX with div, add 'data-key' attribute to store pokemon
    name in listed elements for access during onClick events, add CSS
    className attributes for styling later, and incorporate first-letter
    capitalization function.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,15 +1,19 @@
+import { capitalizeFirst } from "../scripts/utility";
+
 function GameBoard({ pokemon, onClick }) {
   return (
-    Object.keys(pokemon).map((key) => {
+    <div className='game-board'>
+    {Object.keys(pokemon).map((key) => {
       return (
-        <article key={key}>
-          <button type="button" onClick={onClick}>
+        <article className='pokemon-card' key={key}>
+          <button type="button" data-key={key} onClick={onClick}>
             <img src={pokemon[key].sprites.front_default} alt={key} />
-            <h2>{key}</h2>
+            <h2>{capitalizeFirst(key)}</h2>
           </button>
         </article>
       );
-    })
+    })}
+    </div>
   );
 }
 
