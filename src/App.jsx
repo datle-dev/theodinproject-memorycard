88c356f6ca41ad0f34392b6ddf5e5bf1c464fd02
datle-dev <dle.dev@outlook.com>
2024-04-16 06:05:56 -0400

    Add add/subtract counter for number of memory cards
    Create new state variable to track number of pokemon (cards) and two
    buttons to add/subtract from this value. Main fetch function now
    references this state variable to determine how many random pokemon
    to fetch from pokeAPI.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { fetchAll, createRandIntArray, randomizeArrayOrder } from './scripts/uti
 
 function App() {
   const [pokemon, setPokemon] = useState({});
+  const [numPokemon, setNumPokemon] = useState(8);
+  const [indices, setIndices] = useState(createRandIntArray(numPokemon));
   const [order, setOrder] = useState([])
   const [isLoading, setIsLoading] = useState(true);
   const [points, setPoints] = useState(0);
@@ -13,11 +15,9 @@ function App() {
 
   useEffect(() => {
     let pokemonObject = {};
-    let pokemonIndexArray = createRandIntArray(8);
-    let pokemonURLArray = pokemonIndexArray.map((index) => {
+    let pokemonURLArray = indices.map((index) => {
       return `https://pokeapi.co/api/v2/pokemon/${String(index)}`;
     });
-
     fetchAll(pokemonURLArray).then((data) => {
       data.forEach((entry) => {
         pokemonObject[entry.species.name] = entry;
@@ -27,13 +27,28 @@ function App() {
       setOrder(pokemonOrder);
       setIsLoading(false);
     });
-  }, []);
+  }, [indices]);
+
+  let canAdd = numPokemon < 16;
+  let canSubtract = numPokemon > 4;
 
   function handlePokemonSelect(e) {
     let pokemonName = e.target.getAttribute('data-key');
     console.log(pokemonName);
     const pokemonOrder = randomizeArrayOrder(Object.keys(pokemon));
     setOrder(pokemonOrder);
+  }
+
+  function handleNewPokemon(e) {
+    setIndices(createRandIntArray(numPokemon));
+  }
+
+  function handleIncreaseCount(e) {
+    setNumPokemon(numPokemon + 1);
+  }
+
+  function handleDecreaseCount(e) {
+    setNumPokemon(numPokemon - 1);
   }
 
   if (isLoading) {
@@ -49,6 +64,10 @@ function App() {
           </p>
         </header>
         <main>
+          <button type="button" onClick={handleNewPokemon}>New Pokemon</button>
+          <p>Count: {numPokemon}</p>
+          <button type="button" onClick={handleIncreaseCount} disabled={!canAdd}>+</button>
+          <button type="button" onClick={handleDecreaseCount} disabled={!canSubtract}>-</button>
           <ScoreBoard points={points} best={best} />
           <GameBoard pokemon={pokemon} order={order} onClick={handlePokemonSelect} />
         </main>
